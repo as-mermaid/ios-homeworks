96fbdc9c3bee6f1a236f8fac0b6125fdca56b097
Anastasia Smorodinova <stacie_sm@mail.ru>
2023-01-05 19:27:33 +0100

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Anastasia Smorodinova on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
